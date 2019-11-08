ffe87bc69b98e06013004b14af05d960c10bab93
KissRoland <kissrolandkristof@gmail.com>
2019-11-08 14:37:31 +0100

    CRUD READ file

---
+++ mysql/login_read.php
@@ -0,0 +1,72 @@
+<?php
+
+// CRUD - READ
+
+
+
+    $connection = mysqli_connect('localhost', 'root', '', "loginapp");
+    if ($connection) {
+            echo "We are connected";
+            echo "</br>";
+    } else {
+        die("Database connection failed!");
+    }
+
+
+$query = "SELECT * FROM users";
+
+$result =  mysqli_query($connection, $query);
+
+    if(!$result){
+        die('Query FAILED' . mysqli_error());
+    }
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login Page</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</head>
+<body>
+
+<div class="container">
+
+    <div class="col-sm-6">
+        <?php
+
+        while ($row = mysqli_fetch_assoc($result)){
+        ?>
+        <pre>
+        <?php
+            print_r($row);
+        ?>
+        </pre>
+        <?php
+
+        }
+
+
+
+
+        ?>
+
+
+    </div>
+
+
+
+</div>
+
+
+
+
+</body>
+</html>
+
+
