f1a41cb1d8d58655d5d6a243da87f2701345f2a7
KissRoland <kissrolandkristof@gmail.com>
2020-07-14 21:01:10 +0200

    try fix login-form-validate

--- users_functions.php
+++ users_functions.php
@@ -34,3 +34,37 @@ function registerUser($email, $password, $name, $address)
     }
         return true;
 }
+
+function loginUser($email,$password)
+{
+    $conn = require "db.php";
+
+    $query = "SELECT * FROM registration WHERE email = '$email'";
+    $select_login = mysqli_query($conn, $query);
+
+    if (!$select_login) {
+        die("QUERY FAILED" . mysqli_error($conn));
+    }
+
+    $row = mysqli_fetch_assoc($select_login);
+    $hash = $row['password'];
+
+    if (crypt($password, $hash) == $hash) {
+
+        $_SESSION['email'] = $email;
+        $_SESSION['password'] = $password;
+
+        header('Location: http://localhost/phpPractice/');
+        $_SESSION['flash']['success'] = 'bejelentkezés sikeres';
+        echo  $_SESSION['email'];
+        echo "<br>";
+        echo  $_SESSION['password'];
+        return true;
+    }else{
+
+        return false;
+        header('Location: http://localhost/phpPractice/login.php');
+        $_SESSION['flash']['error'] = "Nem sikerult a bejelentkezes. Nem megfelelo adatok !";
+    }
+
+}
